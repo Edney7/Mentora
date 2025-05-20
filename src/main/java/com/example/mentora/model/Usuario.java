3180b4926409f51383fc60c28e70cd99be1e964e
Edney7 <edneiaraujosilva00@gmail.com>
2025-05-20 09:58:42 -0300

    Teste de cadastro de usuário realizado e funcionando.

--- src/main/java/com/example/mentora/model/Usuario.java
+++ src/main/java/com/example/mentora/model/Usuario.java
@@ -12,4 +12,45 @@ public class Usuario {
     private String email;
     private String senha;
     private String tipoUsuario; //Aluno, Professor ou Funcionário da secretaria
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
 }
