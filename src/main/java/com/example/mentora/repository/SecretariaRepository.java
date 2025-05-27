3fafcd476cca6630abe02f8b9d30b3a6f27ebe40
Adriian.js <adriandev.js@gmail.com>
2025-05-27 13:16:33 -0300

    nem sei p qu mudei

--- src/main/java/com/example/mentora/repository/SecretariaRepository.java
+++ src/main/java/com/example/mentora/repository/SecretariaRepository.java
@@ -2,6 +2,8 @@ package com.example.mentora.repository;
 
 import com.example.mentora.model.Secretaria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SecretariaRepository extends JpaRepository<Secretaria, Long> {
 }
