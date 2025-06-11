c925535c0a69a7176a85636c28dae14380e37b24
Marilia <mariliateixeirainacio84@gmail.com>
2025-06-11 11:26:37 -0300

    css lista usuario

--- frontend/src/views/Secretaria/ListaUsuario.js
+++ frontend/src/views/Secretaria/ListaUsuario.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState, useCallback } from "react";
-import "../../styles/ListaUsuario.css";
 import Navbar from "../../components/Navbar";
-
 import {
   listarTodosOsUsuarios,
   desativarUsuario,
